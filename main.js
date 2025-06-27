8100e58af0d65b810aad21bf0e3a1d4e4c7ae4dc
angel123321 <142400313+angel123321@users.noreply.github.com>
2025-06-27 07:55:07 -0300

    .

--- main.js
+++ main.js
@@ -62,5 +62,8 @@ function geraSenha() {
 }
 function classificaSenha(){
     forcaSenha.classList.remove('fraca','media','forte');
+    if (tamanhoSenha > 11){
+forcaSenha.classList.add('forte');
+}
 forcaSenha.classList.add('forte');
 }
